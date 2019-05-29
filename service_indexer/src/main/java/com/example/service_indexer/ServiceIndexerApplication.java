cd030ec460645b5443f401336168d494bee06639
ksereda <k.sereda@softteco.com>
2019-05-29 14:36:17 +0300

    created replicas for service_indexer

--- service_indexer/src/main/java/com/example/service_indexer/ServiceIndexerApplication.java
+++ service_indexer/src/main/java/com/example/service_indexer/ServiceIndexerApplication.java
@@ -2,10 +2,12 @@ package com.example.service_indexer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @EnableAsync
 @SpringBootApplication
 public class ServiceIndexerApplication {
