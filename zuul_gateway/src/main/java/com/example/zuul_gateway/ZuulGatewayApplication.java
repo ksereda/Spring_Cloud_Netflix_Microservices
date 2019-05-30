dd0b4e3f487fbad4df1d5e8bdc787b129122f8b8
ksereda <k.sereda@softteco.com>
2019-05-30 23:06:31 +0300

    added zuul gateway to project

---
+++ zuul_gateway/src/main/java/com/example/zuul_gateway/ZuulGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.example.zuul_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
+
+
